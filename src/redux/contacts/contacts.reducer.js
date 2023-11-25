4a1f7640d33a3ad292c1f4f96f8518b3b0ecbcfd
sidlicki <130383232+sidlicki@users.noreply.github.com>
2023-11-25 03:44:02 +0100

    step 0.5

--- src/redux/contacts/contacts.reducer.js
+++ src/redux/contacts/contacts.reducer.js
@@ -1,30 +1,56 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, isAnyOf } from '@reduxjs/toolkit';
 
-const exampleConacts = [
-  { id: 'id-1', name: 'Joe Biden', number: '+1-800-375-5283' },
-  { id: 'id-2', name: 'Naruhito 徳仁', number: '+81-354-74-97-70' },
-  { id: 'id-3', name: 'Frank Steinmeier', number: '+49-30-28-88-71-28' },
-  { id: 'id-4', name: 'Rishi Sunak', number: ' +44-0-131-556-0023' },
-  { id: 'id-5', name: 'Emmanuel Macron', number: '+33-1-43-06-07-37' },
-  { id: 'id-6', name: 'Sergio Mattarella', number: '+39-06-8200-3641' },
-  { id: 'id-7', name: 'Justin Trudeau', number: '+1-613-230-29-61' },
-  { id: 'id-8', name: 'Angela Merkel', number: '+49-30-18-580' },
-  { id: 'id-9', name: 'Xi Jinping 习近平', number: '+86-10-6307-0911' },
-  { id: 'id-10', name: 'Jair Bolsonaro', number: '+55-61-3429-1555' },
-  { id: 'id-11', name: 'Scott Morrison', number: '+61-2-6271-5111' },
-  { id: 'id-12', name: 'Moon Jae-in 문재인', number: '+82-2-730-5800' },
-  {
-    id: 'id-13',
-    name: 'Volodymyr Zelensky',
-    number: '+380-44-256-67-25',
-  },
-  { id: 'id-14', name: 'Macky Sall', number: '+221-33-839-50-50' },
-  { id: 'id-15', name: 'Tayyip Erdoğan', number: '+90-312-417-05-30' },
-  { id: 'id-16', name: 'Jacinda Ardern', number: '+64-4-817-9999' },
-];
+import axios from 'axios';
+
+export const fetchAllContacts = createAsyncThunk(
+  'contacts/fetchAllContacts',
+  async (_, thunkApi) => {
+    try {
+      const { data } = await axios.get(
+        'https://6560d78d83aba11d99d199f6.mockapi.io/contacts/allContacts'
+      );
+      return data;
+    } catch (err) {
+      return thunkApi.rejectWithValue(err.message);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async (newContact, thunkApi) => {
+    try {
+      const { data } = await axios.post(
+        'https://6560d78d83aba11d99d199f6.mockapi.io/contacts/allContacts',
+        newContact
+      );
+      return data;
+    } catch (err) {
+      return thunkApi.rejectWithValue(err.message);
+    }
+  }
+);
+
+export const removeContact = createAsyncThunk(
+  'contacts/removeContact',
+  async (id, thunkApi) => {
+    try {
+      const { data } = await axios.delete(
+        `https://6560d78d83aba11d99d199f6.mockapi.io/contacts/allContacts/${id}`
+      );
+      return data;
+    } catch (err) {
+      return thunkApi.rejectWithValue(err.message);
+    }
+  }
+);
 
 const initialState = {
-  contacts: exampleConacts,
+  contacts: {
+    items: [],
+    isLoading: false,
+    error: null,
+  },
   filter: '',
   favoriteContacts: [],
 };
@@ -33,32 +59,70 @@ const contactsSlice = createSlice({
   name: 'contacts',
   initialState,
   reducers: {
-    addContact(state, { payload }) {
-      state.contacts = [...state.contacts, payload];
-    },
-    deleteContact(state, { payload }) {
-      state.contacts = state.contacts.filter(contact => contact.id !== payload);
-    },
     filterContacts(state, { payload }) {
       state.filter = payload;
     },
-    addToFavorite(state, { payload }) {
-      state.favoriteContacts = [...state.favoriteContacts, payload];
-    },
-    removeFromFavorite(state, { payload }) {
-      state.favoriteContacts = state.favoriteContacts.filter(
-        contact => contact.id !== payload
-      );
-    },
+    // addContact(state, { payload }) {
+    //   state.contacts = [...state.contacts, payload];
+    // },
+    // deleteContact(state, { payload }) {
+    //   state.contacts = state.contacts.filter(contact => contact.id !== payload);
+    // },
+    // addToFavorite(state, { payload }) {
+    //   state.favoriteContacts = [...state.favoriteContacts, payload];
+    // },
+    // removeFromFavorite(state, { payload }) {
+    //   state.favoriteContacts = state.favoriteContacts.filter(
+    //     contact => contact.id !== payload
+    //   );
+    // },
   },
+  extraReducers: builder =>
+    builder
+      .addCase(fetchAllContacts.fulfilled, (state, { payload }) => {
+        state.contacts.isLoading = false;
+        state.contacts.items = payload;
+      })
+      .addCase(addContact.fulfilled, (state, { payload }) => {
+        state.contacts.isLoading = false;
+        state.contacts.items = [...state.contacts.items, payload];
+      })
+      .addCase(removeContact.fulfilled, (state, { payload }) => {
+        state.contacts.isLoading = false;
+        state.contacts.items = state.contacts.items.filter(
+          contact => contact.id !== payload
+        );
+      })
+      .addMatcher(
+        isAnyOf(
+          fetchAllContacts.pending,
+          addContact.pending,
+          removeContact.pending
+        ),
+        state => {
+          state.contacts.isLoading = true;
+          state.contacts.error = null;
+        }
+      )
+      .addMatcher(
+        isAnyOf(
+          fetchAllContacts.rejected,
+          addContact.rejected,
+          removeContact.rejected
+        ),
+        (state, { payload }) => {
+          state.contacts.isLoading = false;
+          state.contacts.error = payload;
+        }
+      ),
 });
 
 export const {
-  addContact,
-  deleteContact,
+  // addContact,
+  // deleteContact,
   filterContacts,
-  addToFavorite,
-  removeFromFavorite,
+  // addToFavorite,
+  // removeFromFavorite,
 } = contactsSlice.actions;
 
 export const contactsReducer = contactsSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,34 +1,35 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
 
 import { contactsReducer } from './contacts/contacts.reducer';
 
-const contactsConfig = {
-  key: 'contacts',
-  storage,
-  whitelist: ['contacts', 'favoriteContacts'],
-};
+// const contactsConfig = {
+//   key: 'contacts',
+//   storage,
+//   whitelist: ['contacts', 'favoriteContacts'],
+// };
 
 export const store = configureStore({
   reducer: {
-    contactsStore: persistReducer(contactsConfig, contactsReducer),
+    // contactsStore: persistReducer(contactsConfig, contactsReducer),
+    contacts: contactsReducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  // middleware: getDefaultMiddleware =>
+  //   getDefaultMiddleware({
+  //     serializableCheck: {
+  //       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+  //     },
+  //   }),
 });
 
-export const persistor = persistStore(store);
+// export const persistor = persistStore(store);
